033bdd276011fb57c2dfea18613e3fc190ac18c7
ibrahim <ibrahimulusal185@gmail.com>
2022-12-05 20:04:31 +0300

    admin update

--- lessons/admin.py
+++ lessons/admin.py
@@ -6,7 +6,7 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ('title','slug')
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name','descriptionCategory','slug')
+    list_display = ('name','slug')
 
 admin.site.register(models.LessonModels,LessonAdmin)
 admin.site.register(models.Category,CategoryAdmin)
