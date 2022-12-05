0b73916c9578244d33d466508291712abf96d9d2
ibrahim <ibrahimulusal185@gmail.com>
2022-12-05 21:27:55 +0300

    ckeditor eklendi.

--- lessons/models.py
+++ lessons/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils.text import slugify
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 
 class LessonModels(models.Model):
     
     title = models.CharField(max_length=50)
-    description = models.TextField()
+    description = RichTextField()
     create_date = models.DateTimeField(auto_now_add=True,verbose_name='Oluşturulma Tarihi')
     update_date = models.DateTimeField(auto_now=True,verbose_name='Son Güncelleme Tarihi')
     slug = models.SlugField(db_index=True,unique=True,editable=False)
@@ -21,7 +22,7 @@ class LessonModels(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    descriptionCategory = models.TextField()
+    descriptionCategory = RichTextField()
     slug = models.SlugField(db_index=True,editable=False,unique=True)
     
     def save(self,*args,**kwargs):
